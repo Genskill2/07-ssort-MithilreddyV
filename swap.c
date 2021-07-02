8a83c07f7a0e3ca27176a149ed8c5b74cbd709cc
MithilreddyV <84663273+MithilreddyV@users.noreply.github.com>
2021-07-02 06:28:10 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap_max(int a[], int l, int n) {
+    int d = a[n];
+    int e=0;
+    for (int i=n; i<l; i++) {
+        if (a[i] > d) {
+            d = a[i];;
+            e = i;
+        }
+    }
+    if (d!=a[n]) {
+        a[e] = a[n];
+        a[n] = d;
+    }
+}
+
+void ssort(int p[], int m) {
+    m=l-1;
+    for (int i=0;i<m;i++) {
+        swap_max(p,m,i);
+    }
+}
